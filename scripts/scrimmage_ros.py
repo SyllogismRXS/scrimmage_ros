5b532d2004e0e2887e31b3db087bf442e91d6e3c
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2017-08-14 18:40:28 -0400

    initial commit

---
+++ scripts/scrimmage_ros.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import argparse
+
+def main():
+    os.system("scrimmage /home/syllogismrxs/repos/scrimmage_ws/src/scrimmage_ros/missions/ros-ex1.xml")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+    
