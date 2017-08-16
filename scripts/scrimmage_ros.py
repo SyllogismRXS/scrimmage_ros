cbf014a6ac40109b15fddd3c2653099e1e02e280
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2017-08-16 08:54:54 -0400

    two robots working with 2d nav stack

--- scripts/scrimmage_ros.py
+++ scripts/scrimmage_ros.py
@@ -5,8 +5,20 @@ import os
 import argparse
 
 def main():
-    os.system("scrimmage /home/syllogismrxs/repos/scrimmage_ws/src/scrimmage_ros/missions/ros-ex1.xml")
-    return 0
+    parser = argparse.ArgumentParser(description='Python wrapper for scrimmage.')
+    parser.add_argument('mission_file',
+                        type=str,
+                        help='SCRIMMAGE mission file.')
+
+    args, unknown = parser.parse_known_args()
+
+    if not os.path.isfile(args.mission_file):
+        print('Mission file doesn\'t exist: %s' % args.mission_file)
+        return -1
+
+    cmd = "scrimmage " + args.mission_file
+    
+    return os.system(cmd)
 
 if __name__ == '__main__':
     sys.exit(main())
