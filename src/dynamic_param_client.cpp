c9a306bd642a6d013f722850428588783fc9b7b7
James Michael Matthews <james.michael.matthews@gmail.com>
2019-07-31 09:52:49 -0400

    Allow user to pass in a current param generator to be sent to any new dynamic param servers.

--- src/dynamic_param_client.cpp
+++ src/dynamic_param_client.cpp
@@ -11,9 +11,8 @@ namespace scrimmage_ros {
 dynamic_param_client::dynamic_param_client(const std::string &name)
     : name_(name) {}
 
-bool dynamic_param_client::update_dynamic_param_servers() {
-    services_.clear();
-
+bool dynamic_param_client::update_dynamic_param_servers(
+       std::function<void(std::vector<scrimmage_rosConfig>&)> generate_current_config_list) {
     XmlRpc::XmlRpcValue req = "/node";
     XmlRpc::XmlRpcValue res;
     XmlRpc::XmlRpcValue pay;
@@ -27,6 +26,7 @@ bool dynamic_param_client::update_dynamic_param_servers() {
         return false;
     }
 
+    std::vector<std::string> updated_service_names_list;
     for(int i = 0; i < res[2][2].size(); i++) {
         // Get list of services
         std::string gh = res[2][2][i][0].toXml().c_str();
@@ -58,16 +58,45 @@ bool dynamic_param_client::update_dynamic_param_servers() {
         std::size_t set_param_pos = service_name.find("/set_parameters");
         if (set_param_pos != std::string::npos) {
             std::string node_name = service_name.substr(0, set_param_pos);
-            ros::NodeHandle nh(node_name);
-            services_[node_name] =
-                std::make_shared<dynamic_reconfigure::Client<scrimmage_rosConfig>>(name_, nh);
+            updated_service_names_list.push_back(node_name);
         }
     }
+    // Populate the updated client list
+    std::unordered_map<std::string, std::shared_ptr<dynamic_reconfigure::Client<scrimmage_rosConfig>>> updated_services_list;
+    auto move_or_create_client = [&](const std::string& node_name) -> void {
+        // check if this node is already in the list
+        std::unordered_map<std::string,
+          std::shared_ptr<dynamic_reconfigure::Client<scrimmage_rosConfig>>
+          >::iterator found_element;
+        found_element = services_.find(node_name);
+        if (found_element != services_.end()) {
+            // this one already exists, just move it to the new list
+            updated_services_list[node_name] = found_element->second; //shared_ptr copy
+        } else {
+            // this one is new, create it and initialize it
+            ros::NodeHandle nh(node_name);
+            std::shared_ptr<dynamic_reconfigure::Client<scrimmage_rosConfig>> new_client =
+                std::make_shared<dynamic_reconfigure::Client<scrimmage_rosConfig>>(name_, nh);
+            updated_services_list[node_name] = new_client;
+            // initialize it
+            //   get the current configs from the caller
+            std::vector<scrimmage_rosConfig> current_config_list;
+            if(generate_current_config_list) generate_current_config_list(current_config_list);
+            //   send each one to the new client
+            std::for_each(current_config_list.cbegin(), current_config_list.cend(),
+                [&new_client](const scrimmage_rosConfig& config) -> void {new_client->setConfiguration(config);});
+        }
+    };
+    std::for_each(updated_service_names_list.cbegin(), updated_service_names_list.cend(), move_or_create_client);
+    services_.swap(updated_services_list); //save the result
+
     return true;
 }
 
 bool dynamic_param_client::send_config(const scrimmage_rosConfig &config) {
-    auto send_config = [&](auto &s) { return s.second->setConfiguration(config); };
+    auto send_config = [&](auto &s) { 
+      std::cout << "dynamic_param_client::send_config: node_name: " << s.first.c_str() << " param_name: " << config.param_name << " param_value: " << config.param_value << std::endl;
+      return s.second->setConfiguration(config); };
     return std::all_of(services_.begin(), services_.end(), send_config);
 }
 } // namespace scrimmage_ros
